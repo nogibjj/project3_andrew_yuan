fa4caa13127abcb34fef34e9f7cd283f6984b467
Andrew Y <72961739+Xinz-Y@users.noreply.github.com>
2022-11-08 07:01:09 +0000

    finish

--- data_process.py
+++ data_process.py
@@ -1,26 +1,80 @@
 import sqlite3
+import click
 
-conn = sqlite3.connect("covid.db")
-cursor = conn.cursor
 
 # three options to choose
 
 # choice 1 : fetch the data of one country
 cmd1 = """
-    SELECT 
+    SELECT * FROM covidCases 
+    WHERE Country ==?
+"""
 
-
-
+# choice 2 : sort the countries by either total_cases
+cmd2 = """
+    SELECT Country FROM covidCases 
+    ORDER BY Total_Cases DESC
 
 """
 
 
-# choice 2 : sort the countries by either total_cases or total_deaths
+# choice 3: alter the table to have a new column which is the ratio of death-infection
+# sort countires by this new column and return 
+cmd3 = """
+    ALTER TABLE covidCases
+    ADD COLUMN Death_Infection 'float';
+    UPDATE covidCases SET Death_Infection = Total_Deaths/Total_Cases
+"""
 
 
+@click.command()
+@click.option(
+    "--choice",
+    "-c",
+    default=1,
+    required = True,
+    type = int,
+    help = "Choices to do with the db"
+)
+@click.option(
+    "--name",
+    "-n",
+    default = "China",
+    help = "Select which country to return" 
+)
+def main(choice,name):
+    conn = sqlite3.connect("covid.db")
+    cursor = conn.cursor()
+    if choice == 1:
+        print("You choose choice 1 to fetch country data")
+        cursor.execute(cmd1,(name,))
+        result = cursor.fetchall()
+        print(result)
+        pass
+    elif choice == 2:
+        print("You choose choice 2 to sort the countries by total cases")
+        cursor.execute(cmd2)
+        result = cursor.fetchmany(size=25)
+        print(result)
+        pass
+    elif choice ==3:
+        print("You choose choice 3 to sort the countries by ratio of death to infection")
+        cursor.executescript(cmd3)
+        conn.commit()
+        # countries,total_cases,total_deaths= [row[0],row[2],row[3] for row in cursor.execute("SELECT * FROM covidCases")]
+        # count =0
+        # for c in countries:
+        #     cursor.execute("UPDATE MyTable SET Death_Infection = ? WHERE Country = ?",
+        #                 [total_cases[count]/total_deaths[count], c])
+        cursor.execute('SELECT Country FROM covidCases ORDER BY Death_Infection DESC')
+        result = cursor.fetchmany(size=25)
+        print(result)
+        pass
+    else:
+        print('wrong command option')
+        exit()
+        pass
 
-# choice 3: calculate the death-infection rate of each country and sort
-
-
-if __name__ == 'main':
-    
+if __name__ == '__main__':
+    print("++++++++++")
+    main()
